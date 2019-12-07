8a370207e8f68993c78b7b044c2b2fab770e5e37
Nathan Billis <nathanbillis.nathan@gmail.com>
2019-12-07 13:17:42 +0000

    Initial Commit

---
+++ solarsynthTests/solarsynthTests.swift
@@ -0,0 +1,34 @@
+//
+//  solarsynthTests.swift
+//  solarsynthTests
+//
+//  Created by Nathan Billis on 07/12/2019.
+//  Copyright © 2019 Nathan Billis. All rights reserved.
+//
+
+import XCTest
+@testable import solarsynth
+
+class solarsynthTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
